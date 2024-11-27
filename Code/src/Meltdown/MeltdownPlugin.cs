2e67a0661f9aff7bf18162920eb69d8bcd78818e
pasalvetti <pasalvetti@gmail.com>
2024-11-27 01:02:54 +0100

    Centralize management of the heating flag and the generated flux

--- Code/src/Meltdown/MeltdownPlugin.cs
+++ Code/src/Meltdown/MeltdownPlugin.cs
@@ -15,13 +15,20 @@ namespace Meltdown
     internal class MeltdownPlugin
     {
 
-        private static void SetIsHeating(PartComponentModule __instance, bool isHeating)
+        /**
+         * Sets the 'isHeating' flag, used to mark the part as heating or not when calculating the radiators' influence.
+         * Also sets the flux (in kW). If no flux is to be set, use 0.0 (only to be used if the stock game already generates the flux).
+         **/
+        private static void SetThermalFluxData(PartComponentModule __instance, bool isHeating, double flux)
         {
             if (__instance.Part.TryGetModule<PartComponentModule_Thermal>(out PartComponentModule_Thermal thermalComponent))
             {
                 if (thermalComponent._dataThermal == null) return;
-                //System.Diagnostics.Debug.Write("SetIsHeating: thermal found");
                 thermalComponent._dataThermal.isHeating = isHeating;
+                if (flux != 0.0)
+                {
+                    thermalComponent.SetFlux(flux);
+                }
             }
         }
 
@@ -46,7 +53,7 @@ namespace Meltdown
         [HarmonyPostfix]
         public static void OnUpdatePostFix(double universalTime, PartComponentModule_Generator __instance)
         {
-            SetIsHeating(__instance, true); // a generator is always heating
+            SetThermalFluxData(__instance, true, 0.0); // a generator is always heating. The flux is 0.0 because it's already set in the stock module.
         }
         
     
@@ -83,8 +90,9 @@ namespace Meltdown
             if (__instance._dataResourceConverter.ConverterIsActive && __instance._dataResourceConverter.FluxGenerated > 0.0)
             {
                 __instance.part.Model.ThermalData.OtherFlux = __instance._dataResourceConverter.FluxGenerated * (double)__instance._dataResourceConverter.conversionRate.GetValue();
-            } 
-            SetIsHeating(__instance._componentModule, __instance._dataResourceConverter.ConverterIsActive && __instance._dataResourceConverter.conversionRate.GetValue() > 0); // marks as heating if the converter is on and has a rate > 0
+            }
+            /* Marks as heating if the converter is on and has a rate > 0. The flux is 0.0 because it's already set in the stock module. */
+            SetThermalFluxData(__instance._componentModule, __instance._dataResourceConverter.ConverterIsActive && __instance._dataResourceConverter.conversionRate.GetValue() > 0, 0.0);
 
             //System.Diagnostics.Debug.Write("Module_ResourceConverter.ThermalUpdatePostFix: otherFlux=" + __instance.part.Model.ThermalData.OtherFlux);
         }
@@ -291,15 +299,14 @@ namespace Meltdown
 
         [HarmonyPatch(typeof(PartComponentModule_Command), nameof(PartComponentModule_Command.OnUpdate))]
         [HarmonyPostfix]
-        public static void OnUpdatePostFix(double universalTime, double deltaUniversalTime, PartComponentModule_Command __instance)
+        public static void OnUpdatePostFix(PartComponentModule_Command __instance)
         {
-            if (__instance.Part.TryGetModule<PartComponentModule_Thermal>(out PartComponentModule_Thermal thermalModule))
-            {
-                if (thermalModule._dataThermal == null) return;
-                thermalModule.SetFlux(10);
-                //System.Diagnostics.Debug.Write("isGeneretingHeat: " + part.PartName + " " + part.GlobalId + " yes !");
-                //return thermalModule._dataThermal.isHeating;
-            }
+            SetThermalFluxData(__instance, true, 10); // a command pod is always heating
+            //if (__instance.Part.TryGetModule<PartComponentModule_Thermal>(out PartComponentModule_Thermal thermalModule))
+            //{
+            //    if (thermalModule._dataThermal == null) return;
+            //    thermalModule.SetFlux(10);
+            //}
         }
 
 
