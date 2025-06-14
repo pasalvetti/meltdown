5fca62488c9339757841c540304698ba0c64e778
pasalvetti <pasalvetti@gmail.com>
2025-06-14 13:02:44 +0200

    Fix engine with generator not displaying in VAB

--- Code/src/Meltdown/Patches/GeneratorPatches.cs
+++ Code/src/Meltdown/Patches/GeneratorPatches.cs
@@ -8,16 +8,6 @@ namespace Meltdown.Patches
     internal class GeneratorPatches
     {
         /**
-         * Enables autoshutdown. Useful?
-         **/
-        [HarmonyPatch(typeof(Module_Generator), nameof(Module_Generator.OnInitialize))]
-        [HarmonyPrefix]
-        static public void OnInitializePreFix(Module_Generator __instance)
-        {
-            __instance.dataGenerator.AutoShutdown = true;
-        }
-
-        /**
          * Marks the generator as a heat-generating part and enable heat generation for generators.
          **/
         [HarmonyPatch(typeof(Module_Generator), nameof(Module_Generator.ThermalUpdate))]
@@ -29,17 +19,13 @@ namespace Meltdown.Patches
             double rate = 0.0;
             if (isEngine)
             {
-                //System.Diagnostics.Debug.Write("[Meltdown] Module_Generator.ThermalUpdate: " + __instance.part.Model.Name + " " + __instance.part.Model.GlobalId + "/EngineModule=" + __instance.EngineModule);
                 if (__instance.EngineModule != null) // for cornet, trumpet and tuba engines
                 {
-                    //System.Diagnostics.Debug.Write("[Meltdown] Module_Generator.ThermalUpdate: " + __instance.part.Model.Name + " " + __instance.part.Model.GlobalId + "/throttleSetting=" + __instance.EngineModule.throttleSetting);
                     rate = (double)__instance.EngineModule.throttleSetting;
                 }
-                //System.Diagnostics.Debug.Write("[Meltdown] Module_Generator.ThermalUpdate: " + __instance.part.Model.Name + " " + __instance.part.Model.GlobalId + "/_engineStatus=" + __instance._engineStatus);
                 if (__instance._engineStatus != null) // for other engines
                 {
                     rate = (double)__instance._engineStatus.normalizedOutput;
-                    //System.Diagnostics.Debug.Write("[Meltdown] Module_Generator.ThermalUpdate: " + __instance.part.Model.Name + " " + __instance.part.Model.GlobalId + "/normalizedOutput=" + __instance._engineStatus.normalizedOutput);
                 }
             } else
             {
